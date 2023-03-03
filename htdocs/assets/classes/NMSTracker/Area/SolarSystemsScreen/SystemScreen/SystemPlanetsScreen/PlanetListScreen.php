42ebdebd89b17c0120f4ba86db2b667eae77f309
Mistralys <s.mordziol@gmail.com>
2023-03-03 17:06:56 +0100

    Using the new planet list trait.

--- htdocs/assets/classes/NMSTracker/Area/SolarSystemsScreen/SystemScreen/SystemPlanetsScreen/PlanetListScreen.php
+++ htdocs/assets/classes/NMSTracker/Area/SolarSystemsScreen/SystemScreen/SystemPlanetsScreen/PlanetListScreen.php
@@ -5,18 +5,13 @@ declare(strict_types=1);
 namespace NMSTracker\Area\SolarSystemsScreen\SystemScreen\SystemPlanetsScreen;
 
 use Application_Admin_Area_Mode_Submode_Action_CollectionList;
-use NMSTracker\Planets\PlanetRecord;
-use DBHelper_BaseCollection;
-use DBHelper_BaseFilterCriteria_Record;
-use DBHelper_BaseRecord;
+use NMSTracker\Interfaces\Admin\PlanetListScreenInterface;
+use NMSTracker\Interfaces\Admin\PlanetListScreenTrait;
 use NMSTracker\Area\SolarSystemsScreen\SystemScreen;
-use NMSTracker\ClassFactory;
 use NMSTracker\Interfaces\Admin\ViewSystemScreenInterface;
 use NMSTracker\Interfaces\Admin\ViewSystemScreenTrait;
 use NMSTracker\Planets\PlanetFilterCriteria;
-use NMSTracker\PlanetsCollection;
 use NMSTracker\SolarSystemsCollection;
-use NMSTracker_User;
 use UI;
 
 /**
@@ -25,19 +20,14 @@ use UI;
  */
 class PlanetListScreen
     extends Application_Admin_Area_Mode_Submode_Action_CollectionList
-    implements ViewSystemScreenInterface
+    implements
+    ViewSystemScreenInterface,
+    PlanetListScreenInterface
 {
     use ViewSystemScreenTrait;
+    use PlanetListScreenTrait;
 
     public const URL_NAME = 'list';
-    public const COL_LABEL = 'label';
-    public const COL_TYPE = 'type';
-    public const COL_SENTINELS = 'sentinels';
-    public const COL_SCAN_COMPLETE = 'scan_complete';
-    public const COL_OUTPOSTS = 'outposts';
-    public const COL_RESOURCES = 'resources';
-    public const COL_FAUNA = 'fauna';
-    public const COL_PLANETFALL = 'planetfall';
 
     public function getURLName() : string
     {
@@ -54,18 +44,15 @@ class PlanetListScreen
         return t('System planets overview');
     }
 
-    /**
-     * @return PlanetsCollection
-     */
-    protected function createCollection() : DBHelper_BaseCollection
-    {
-        return ClassFactory::createPlanets();
-    }
-
     protected function _handleHelp() : void
     {
         $this->renderer
             ->setSubtitle($this->getTitle());
+    }
+
+    public function getPlanetFilters() : PlanetFilterCriteria
+    {
+        return $this->filters;
     }
 
     protected function _handleSidebar() : void
@@ -77,66 +64,19 @@ class PlanetListScreen
         parent::_handleSidebar();
     }
 
-    protected function getEntryData(DBHelper_BaseRecord $record, DBHelper_BaseFilterCriteria_Record $entry) : array
-    {
-        if($record instanceof PlanetRecord)
-        {
-            return array(
-                self::COL_LABEL => sb()->add($record->getLabelLinked())->add($record->getMoonIcon()),
-                self::COL_TYPE => $record->getType()->getLabelLinked(),
-                self::COL_FAUNA => $record->getFaunaAmountPretty(true),
-                self::COL_SENTINELS => $record->getSentinelLevel()->getLabelLinked(),
-                self::COL_SCAN_COMPLETE => UI::prettyBool($record->isScanComplete())->makeYesNo(),
-                self::COL_PLANETFALL => UI::prettyBool($record->isPlanetFallMade())->makeYesNo(),
-                self::COL_OUTPOSTS => sb()->link(
-                    $record->countOutpostsPretty(),
-                    $record->getAdminOutpostsURL()
-                ),
-                self::COL_RESOURCES => $record->getResourceFilters()->countItems()
-            );
-        }
-
-        return array();
-    }
-
     protected function configureFilters() : void
     {
         $this->filters->selectSolarSystem($this->getSolarSystem());
     }
 
-    protected function configureColumns() : void
-    {
-        $this->grid->addColumn(self::COL_LABEL, t('Name'))
-            ->setSortable(true, $this->filters->getColLabel());
-
-        $this->grid->addColumn(self::COL_TYPE, t('Type'));
-
-        $this->grid->addColumn(self::COL_SENTINELS, t('Sentinels'));
-
-        $this->grid->addColumn(self::COL_SCAN_COMPLETE, t('Scan complete?'))
-            ->alignCenter();
-
-        $this->grid->addColumn(self::COL_PLANETFALL, t('Planet-fall?'))
-            ->alignCenter();
-
-        $this->grid->addColumn(self::COL_FAUNA, t('Fauna'))
-            ->alignRight();
-
-        $this->grid->addColumn(self::COL_OUTPOSTS, t('Outposts'))
-            ->alignRight();
-
-        $this->grid->addColumn(self::COL_RESOURCES, t('Resources'))
-            ->alignRight();
-
-        $this->grid->enableLimitOptionsDefault();
-        $this->grid->addHiddenVar(SolarSystemsCollection::PRIMARY_NAME, $this->getSolarSystem()->getID());
-    }
-
     protected function configureActions() : void
     {
-        $this->filterSettings->addHiddenVar(
-            SolarSystemsCollection::PRIMARY_NAME,
-            (string)$this->getSolarSystem()->getID()
+    }
+
+    public function getPersistVars() : array
+    {
+        return array(
+            SolarSystemsCollection::PRIMARY_NAME => $this->getSolarSystem()->getID()
         );
     }
 
