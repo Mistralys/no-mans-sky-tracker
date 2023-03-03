696feed5ce622cfdde7bc14fda1b89c75dc165a4
Mistralys <s.mordziol@gmail.com>
2023-03-03 20:25:45 +0100

    Updated to use the planets list trait.

--- htdocs/assets/classes/NMSTracker/Area/PlanetsScreen/PlanetsListScreen.php
+++ htdocs/assets/classes/NMSTracker/Area/PlanetsScreen/PlanetsListScreen.php
@@ -27,19 +27,14 @@ use UI_Renderable_Interface;
  * @property PlanetsScreen $area
  * @property PlanetFilterCriteria $filters
  */
-class PlanetsListScreen extends Application_Admin_Area_Mode_CollectionList
+class PlanetsListScreen
+    extends Application_Admin_Area_Mode_CollectionList
+    implements NMSTracker\Interfaces\Admin\PlanetListScreenInterface
 {
+    use NMSTracker\Interfaces\Admin\PlanetListScreenTrait;
+
     public const URL_NAME = 'planets-list';
     public const GRID_NAME = 'global-planets-list';
-
-    public const COL_LABEL = 'label';
-    public const COL_SYSTEM = 'system';
-    public const COL_TYPE = 'type';
-    public const COL_SENTINELS = 'sentinels';
-    public const COL_SCAN = 'scan';
-    public const COL_OUTPOSTS = 'outposts';
-    public const COL_PLANETFALL = 'planetfall';
-    public const COL_FAUNA = 'fauna';
 
     public function getURLName() : string
     {
@@ -51,76 +46,18 @@ class PlanetsListScreen extends Application_Admin_Area_Mode_CollectionList
         return self::GRID_NAME;
     }
 
-    /**
-     * @return PlanetsCollection
-     */
-    protected function createCollection() : DBHelper_BaseCollection
+    public function getBackOrCancelURL() : string
     {
-        return ClassFactory::createPlanets();
+        return APP_URL;
     }
 
-    /**
-     * @param DBHelper_BaseRecord $record
-     * @param DBHelper_BaseFilterCriteria_Record $entry
-     * @return array|number[]|string[]|UI_Renderable_Interface[]
-     * @throws ClassNotExistsException
-     * @throws ClassNotImplementsException
-     * @throws ConvertHelper_Exception
-     */
-    protected function getEntryData(DBHelper_BaseRecord $record, DBHelper_BaseFilterCriteria_Record $entry) : array
+    public function getPlanetFilters() : PlanetFilterCriteria
     {
-        $planet = ClassHelper::requireObjectInstanceOf(PlanetRecord::class, $record);
-        $system = $planet->getSolarSystem();
-
-        return array(
-            self::COL_LABEL => $planet->getLabelLinked(),
-            self::COL_SYSTEM => $system->getLabelLinked(),
-            self::COL_TYPE => $planet->getType()->getLabelLinked(),
-            self::COL_SENTINELS => $planet->getSentinelLevel()->getLabelLinked(),
-            self::COL_SCAN => UI::prettyBool($planet->isScanComplete())->makeYesNo(),
-            self::COL_FAUNA => $planet->getFaunaAmountPretty(true),
-            self::COL_PLANETFALL => UI::prettyBool($planet->isPlanetFallMade())->makeYesNo(),
-            self::COL_OUTPOSTS => sb()->link(
-                (string)$planet->countOutposts(),
-                $planet->getAdminOutpostsURL()
-            )
-        );
-    }
-
-    protected function configureColumns() : void
-    {
-        $this->grid->addColumn(self::COL_LABEL, t('Name'))
-            ->setSortable(true, $this->filters->getColLabel());
-
-        $this->grid->addColumn(self::COL_SYSTEM, t('Solar system'));
-
-        $this->grid->addColumn(self::COL_TYPE, t('Type'));
-
-        $this->grid->addColumn(self::COL_SENTINELS, t('Sentinels'));
-
-        $this->grid->addColumn(self::COL_SCAN, t('Scan complete?'))
-            ->alignCenter();
-
-        $this->grid->addColumn(self::COL_PLANETFALL, t('Planet-fall?'))
-            ->alignCenter();
-
-        $this->grid->addColumn(self::COL_FAUNA, t('Fauna'))
-            ->alignRight();
-
-        $this->grid->addColumn(self::COL_OUTPOSTS, t('Outposts'))
-            ->alignRight();
-
-        $this->grid->enableColumnControls(6);
+        return $this->filters;
     }
 
     protected function configureActions() : void
     {
-
-    }
-
-    public function getBackOrCancelURL() : string
-    {
-        return APP_URL;
     }
 
     public function isUserAllowed() : bool
