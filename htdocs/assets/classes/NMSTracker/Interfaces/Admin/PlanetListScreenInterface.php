626ad2c37c292de0ae04d2f062bca3bc7378f12f
Mistralys <s.mordziol@gmail.com>
2023-03-03 20:25:19 +0100

    Added system column constant.

--- htdocs/assets/classes/NMSTracker/Interfaces/Admin/PlanetListScreenInterface.php
+++ htdocs/assets/classes/NMSTracker/Interfaces/Admin/PlanetListScreenInterface.php
@@ -12,6 +12,7 @@ interface PlanetListScreenInterface extends CollectionListInterface
     public const COL_RESOURCES = 'resources';
     public const COL_OUTPOSTS = 'outposts';
     public const COL_TYPE = 'type';
+    public const COL_SYSTEM = 'system';
     public const COL_SCAN_COMPLETE = 'scan_complete';
     public const COL_PLANETFALL = 'planetfall';
     public const COL_SENTINELS = 'sentinels';
